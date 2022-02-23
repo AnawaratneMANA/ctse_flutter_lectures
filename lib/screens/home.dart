34045b9c43f9b85257328094ad56f9b84deacc69
AnawaratneMANA <it19086408@my.sliit.lk>
2022-02-23 15:34:45 +0530

    Calling method Updated to call private Method.

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -32,7 +32,7 @@ class _HomeState extends State<Home> {
         ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
-        onPressed: onIncrement,
+        onPressed: _onIncrement,
       ),
     );
   }
