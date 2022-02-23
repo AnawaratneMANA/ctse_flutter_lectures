87a93f72aba7a468d000f1229c3efea1b0c38aa3
AnawaratneMANA <it19086408@my.sliit.lk>
2022-02-23 16:45:01 +0530

    Added the color management class (Inherited Widget)

---
+++ lib/components/color_management.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ColorManager extends InheritedWidget {
+  ColorManager({required Widget child}) : super(child: child);
+
+  @override
+  bool updateShouldNotify(covariant InheritedWidget oldWidget) {
+    // TODO: implement updateShouldNotify
+    throw UnimplementedError();
+  }
+  
+}
